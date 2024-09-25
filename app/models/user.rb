65917138d3a4986e520997e37fd5c7eada9dea8c
Miya096jp <miyawaki.yske@gmail.com>
2024-09-25 13:09:01 +0900

    Userモデルに画像形式によるvalidationを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :image
+  validate :image_content_type
+
+
+  def image_content_type
+    if image.attached? && !image.content_type.in?(%w[image/jpeg image/png image/gif])
+      errors.add(:image, '：ファイル形式が、JPEG, PNG, GIF以外になってます。')
+    end
+  end
 end
