3e2baf984b1d20542b04f6d334ea403a45414933
Miya096jp <miyawaki.yske@gmail.com>
2024-09-28 17:14:26 +0900

    commentコントローラ設定, ルーティング設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,58 @@
 class CommentsController < ApplicationController
+  before_action :set_commentable
+  before_action :set_comment, only: [:edit, :update, :destroy]
+
+def create
+  @comment = @commentable.comments.build(comment_params)
+  @comment.user = current_user
+  if @comment.save
+    redirect_to @commentable, notice: 'Comment was successfully posted.'
+  else
+    render_commentable_show
+  end
+end
+
+def edit
+end
+
+def update
+  if @comment.update(comment_params)
+    redirect_to @commentable, notice: 'Comment was successfully updated.'
+  else
+    render :edit, status: :unprocessable_entity
+  end
+end
+
+def destroy
+  @comment.destroy
+  redirect_to @commentable, notice: 'Comment was successfully deleted.'
+end
+
+  private
+
+def comment_params
+  params.require(:comment).permit(:title, :body)
+end
+
+def set_commentable
+  if params[:book_id]
+    @commentable = Book.find(params[:book_id])
+  elsif params[:report_id]
+    @commentable = Report.find(params[:report_id])
+  end
+end
+
+def set_comment
+  @comment = @commentable.comments.find(params[:id])
+end
+
+def render_commentable_show
+  if @commentable.is_a?(Book)
+    @book = @commentable
+    render 'books/show'
+  elsif @commentable.is_a?(Report)
+    @report = @commentable
+    render 'reports/show'
+  end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'reports/index'
-  get 'reports/show'
-  get 'reports/edit'
+  resources :books do
+    resources :comments, only: [:create, :edit, :update, :destroy]
+  end
+
+  resources :reports do
+    resources :comments, only: [:create, :edit, :update, :destroy]
+  end
+
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
   resources :users, only: %i(index show)
-  resources :reports
 end
