a9f59c0b82f5a1aa29d4c751628c04d957e2d0ca
Miya096jp <miyawaki.yske@gmail.com>
2024-09-28 16:08:51 +0900

    reportのcrudを実装

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,52 @@
+class ReportsController < ApplicationController
+  def index
+    @reports = Report.all
+  end
+
+  def show
+    @report = set_report
+  end
+
+  def new
+    @report = Report.new
+  end
+
+  def edit
+    @report = set_report
+  end
+
+  def create
+    @report = current_user.reports.new(report_params)
+
+    if @report.save
+      redirect_to @report, notice: "Report was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @report = set_report
+
+    if @report.update(report_params)
+      redirect_to @report, notice: "Report was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @report.destroy!
+    redirect_to reports_path, status: :see_other, notice: "Report was successfully destoroyed."
+  end
+
+  private
+
+  def set_report
+    @report = Report.find(params[:id])
+  end
+
+  def report_params
+    params.require(:report).permit(:title, :body, :user_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'reports/index'
+  get 'reports/show'
+  get 'reports/edit'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books
   resources :users, only: %i(index show)
+  resources :reports
 end
