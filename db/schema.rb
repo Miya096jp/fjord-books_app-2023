9fd9d98ccfa418475b5e8fbc5bf8de13cf6f8176
Miya096jp <miyawaki.yske@gmail.com>
2024-10-06 15:57:15 +0900

    reportsテーブルのuser_idコラムにNOT NULL制約、index、参照整合性制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_28_071828) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_06_065011) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -65,7 +65,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_28_071828) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -87,4 +88,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_28_071828) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "users"
+  add_foreign_key "reports", "users"
 end
