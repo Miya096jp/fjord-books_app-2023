62c28564ee376665af44cb61ad4818bedc85bbb8
Miya096jp <miyawaki.yske@gmail.com>
2024-10-06 16:13:22 +0900

    commentsテーブルのtitleとbodyにNOT NULL制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_06_070042) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_06_071034) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -49,8 +49,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_06_070042) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "title", null: false
+    t.text "body", null: false
     t.string "commentable_type", null: false
     t.integer "commentable_id", null: false
     t.integer "user_id", null: false
