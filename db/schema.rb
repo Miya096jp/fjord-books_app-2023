b4cba87afa6d942154ba18d7dca6d6f3ef42ea7a
Miya096jp <miyawaki.yske@gmail.com>
2024-10-06 16:04:39 +0900

    reportsテーブルのtitleとbodyにNOT NULL制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_06_065011) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_06_070042) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -61,8 +61,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_06_065011) do
   end
 
   create_table "reports", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "title", null: false
+    t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
