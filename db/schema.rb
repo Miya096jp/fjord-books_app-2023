779e14b51d80fdd8929e97acecea4e17d0a7381a
Miya096jp <miyawaki.yske@gmail.com>
2024-10-11 11:14:46 +0900

    mentionsテーブルの属性にNOT NULL制約を追加/メンション元のタイトルをハイパーリンクにした

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_02_052203) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_11_014731) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -60,8 +60,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_02_052203) do
   end
 
   create_table "mentions", force: :cascade do |t|
-    t.integer "mention_id"
-    t.integer "mentioned_id"
+    t.integer "mention_id", null: false
+    t.integer "mentioned_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["mention_id", "mentioned_id"], name: "index_mentions_on_mention_id_and_mentioned_id", unique: true
