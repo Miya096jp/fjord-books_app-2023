c19100942e98b7f7ffd95cd6e3acc2aef7ace907
Miya096jp <miyawaki.yske@gmail.com>
2024-10-28 09:14:06 +0900

    userモデルのname_or_emailメソッドのユニットテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#name_or_email' do
+    user = User.new(name: '', email: 'anna@example.com')
+    assert_equal 'anna@example.com', user.name_or_email
+
+    user.name = 'anna'
+    assert_equal 'anna', user.name_or_email
+  end
 end
