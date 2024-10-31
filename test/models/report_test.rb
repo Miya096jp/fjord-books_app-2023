6758a6bb8635fa0a5eb40d1cb10e9533e7ab4053
Miya096jp <miyawaki.yske@gmail.com>
2024-10-31 09:35:03 +0900

    user.rb, report.rbのインスタンスメソッドに対するユニットテストの追加/ユーザー登録、日報のCRUD機能のシステムテスト追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,21 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @anna = users(:anna)
+    @report = reports(:first_report)
+  end
+
+  test '#editable? returns true for the report owner' do
+    assert @report.editable?(@anna)
+  end
+
+  test '#editable? returns false for a different user' do
+    other_user = users(:bob)
+    assert_not @report.editable?(other_user)
+  end
+
+  test '#created_on retuns true when expected date and actual date are equal' do
+    assert_equal Date.parse('Sun, 20 Oct 2024'), @report.created_on
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,11 +3,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test '#name_or_email' do
-    user = User.new(name: '', email: 'anna@example.com')
-    assert_equal 'anna@example.com', user.name_or_email
+  # test '#name_or_email' do
+  #   user = User.new(name: '', email: 'anna@example.com')
+  #   assert_equal 'anna@example.com', user.name_or_email
 
-    user.name = 'anna'
-    assert_equal 'anna', user.name_or_email
-  end
+  #   user.name = 'anna'
+  #   assert_equal 'anna', user.name_or_email
+  # end
 end
