26d56a25256088499462a494d66944cf7e28c9f2
Miya096jp <miyawaki.yske@gmail.com>
2024-11-07 15:14:52 +0900

    save_mentionsのテストを修正, factories/reports.rbのテストデータ修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -5,7 +5,11 @@ require 'test_helper'
 class ReportTest < ActiveSupport::TestCase
   setup do
     @anna = FactoryBot.create(:anna)
+    @bob = FactoryBot.create(:bob)
     @report = FactoryBot.create(:report, user: @anna)
+    FactoryBot.create(:report, :mentioned_report1, user: @bob)
+    FactoryBot.create(:report, :mentioned_report2, user: @bob)
+    FactoryBot.create(:report, :mentioned_report3, user: @bob)
   end
 
   test '#editable? returns true for the report owner' do
@@ -13,7 +17,7 @@ class ReportTest < ActiveSupport::TestCase
   end
 
   test '#editable? returns false for a different user' do
-    other_user = FactoryBot.create(:bob)
+    other_user = @bob
     assert_not @report.editable?(other_user)
   end
 
@@ -21,7 +25,13 @@ class ReportTest < ActiveSupport::TestCase
     assert_equal Date.parse('Sun, 20 Oct 2024'), @report.created_on
   end
 
-  test '#save_mentions returns true when report is saved' do
-    assert @report.save
+  test '#save_mentions adds new mentions to report_mention table' do
+    @report.save!
+    assert_equal [2, 3], @report.mentioning_reports.map(&:id)
+  end
+
+  test '#save_mentions updates mentions when content is changed' do
+    @report.update!(title: 'Updated', content: 'http://localhost:3000/reports/3とhttp://localhost:3000/reports/4をメンション', user: @anna)
+    assert_equal [3, 4], @report.mentioning_reports.map(&:id)
   end
 end
